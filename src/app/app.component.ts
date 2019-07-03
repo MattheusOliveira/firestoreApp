5f92b6794d90abcb4a5bf8fc0c0b03f66348b44d
MattheusOliveira <mattheus.demoura@gmail.com>
2019-07-03 12:48:17 -0300

    studing firestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as firebase from "firebase";
+import { FirebaseConfig } from "../environments/firebase.config"
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firestoreApp';
+  private db: any;
+
+  constructor() {
+      firebase.initializeApp(FirebaseConfig.firebase);
+      this.db = firebase.firestore();
+
+      this.db.collection('usuarios').where('nome','==', 'matheus')
+          .get().then((snapshot) => {
+            console.log(snapshot)
+            snapshot.forEach((doc) => {
+              console.log(snapshot);
+              console.log(doc.id, doc.data());
+            });
+      }).catch((err) => console.log(err));
+  }
 }
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,11 @@
+export const FirebaseConfig = {
+    firebase: {
+      apiKey: 'AIzaSyDv-RDl3tZGkkYuToN4qc4PUKe2OxfGrNc',
+      authDomain: 'firestore-test-4186b.firebaseapp.com',
+      databaseURL: 'https://firestore-test-4186b.firebaseio.com',
+      projectId: 'firestore-test-4186b',
+      storageBucket: 'firestore-test-4186b.appspot.com',
+      messagingSenderId: '996111535395',
+      appId: '1:996111535395:web:86f289da939ffd9c'
+    }
+};
